d40a5c1f668e33c797f062c550c1a874edd79772
Simon Emms <simon@simonemms.com>
2016-11-05 21:10:58 +0000

    Made the notifications a proper strategy pattern
    I was having a look at this and realised there was too much messaging stuff in the implementation of the
    notifications. The message is now created inside the notification class and that is sent to the individual
    notifier. This is more correct implementation of the strategy pattern (I don't really know what the fuck
    I was thinking when I wrote it originally)

--- notifications/slack.js
+++ notifications/slack.js
@@ -10,15 +10,10 @@
 
 /* Third-party modules */
 var _ = require("lodash");
-var async = require("async");
 var request = require("request");
 
 
 /* Files */
-
-
-var red = "#CF1328";
-var green = "#46A95B";
 
 
 function Slack (webhookUrl) {
@@ -31,7 +26,23 @@ function Slack (webhookUrl) {
 _.extend(Slack.prototype, {
 
 
-    publish: function (message, cb) {
+    send: function (data, cb) {
+
+        var message = {
+            attachments: [{
+                fallback: data.message,
+                color: data.status ? "#46A95B" : "#CF1328",
+                fields: [{
+                    title: data.gatewayId,
+                    value: data.message,
+                    short: false
+                }, {
+                    title: "Log",
+                    value: JSON.stringify(data.log, null, 2),
+                    short: false
+                }]
+            }]
+        };
 
         request.post({
             url: this._webhookUrl,
@@ -39,56 +50,8 @@ _.extend(Slack.prototype, {
             json: true
         }, cb);
 
-    },
-
-
-    send: function (gatewayId, data, log, cb) {
-
-        var self = this;
-
-        var messages = [];
-
-        if (data.downlinkStatus !== data.downlinkPrevious) {
-            messages.push(Slack.writeMessage(gatewayId, "downlink", data.downlinkStatus, log));
-        }
-
-        if (data.uplinkStatus !== data.uplinkPrevious) {
-            messages.push(Slack.writeMessage(gatewayId, "uplink", data.uplinkStatus, log));
-        }
-
-        async.each(messages, function (message, cb) {
-            self.publish(message, cb);
-        }, cb);
-
     }
 
-
-});
-
-
-_.extend(Slack, {
-
-    writeMessage: function (gatewayId, key, status, log) {
-
-        var message = "The " + key + " on gateway '" + gatewayId + "' has " + (status ? "come back up" : "gone down");
-
-        return {
-            attachments: [{
-                fallback: message,
-                color: status ? green : red,
-                fields: [{
-                    title: gatewayId,
-                    value: message,
-                    short: false
-                }, {
-                    title: "Log",
-                    value: JSON.stringify(log, null, 2),
-                    short: false
-                }]
-            }]
-        };
-
-    }
 
 });
 
